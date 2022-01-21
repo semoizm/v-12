b5c7402c8d0ac3583e9ee4754e7dfee229a53dc6
Semih <91805265+semoizm@users.noreply.github.com>
2022-01-21 23:48:52 +0300

    Add files via upload

---
+++ komutlar/boskomut.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js"),
+client = new Discord.Client(),
+ayarlar = require("../ayarlar.json"),
+db = require('quick.db');
+module.exports.run = async (client, message, args) => {
+
+    }  
+  
+  
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ['boskomuts', 'boskomut2'], 
+  permLevel: 0
+};
+exports.help = {
+  name: 'boskomutasddsa', 
+  description: "Komut Örneği.", 
+  usage: 'boskomutasddsa'
+
+};
